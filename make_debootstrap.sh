b3593a8b4979dc4e4c1edc4ed0599ac36bc99afe
Sylvain Leroy <sylvain@unmondelibre.fr>
2013-05-03 00:55:40 +0200

    updated version of the debootstrap script. (tested until additionnals packages installation)

--- make_debootstrap.sh
+++ make_debootstrap.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env sh
 
+set -x
+set -e
+
 #############
 # VARIABLES #
 #############
@@ -12,31 +15,48 @@ LINUX_DIR=../linux-stable/
 # MAIN #
 ########
 
+# create the chroot
 mkdir -p chroot-armhf
 cd chroot-armhf
-sudo /usr/sbin/debootstrap --foreign --arch armhf wheezy .
-#cp /usr/bin/qemu-arm-static usr/bin
-sudo LC_ALL=C LANGUAGE=C LANG=C chroot . /debootstrap/debootstrap --second-stage
+#sudo /usr/sbin/debootstrap --foreign --arch armhf wheezy .
+
+# that command is usefull to run target host binaries (ARM) on the build host  (x86)
+sudo cp /usr/bin/qemu-arm-static usr/bin
+
+# if you use grsecurity on build host, you should uncomment that line
+#sudo /sbin/paxctl -cm usr/bin/qemu-arm-static
+#sudo /sbin/paxctl -cpexrms usr/bin/qemu-arm-static
+
+# debootstrap second stage and packages configuration
+#sudo LC_ALL=C LANGUAGE=C LANG=C chroot . /debootstrap/debootstrap --second-stage
 sudo LC_ALL=C LANGUAGE=C LANG=C chroot . dpkg --configure -a
 
 # set root password
 sudo chroot . passwd
 
 # set hostname
-echo "$HOSTNAME" > etc/hostname
+echo -n "Please enter the hostname of the host: "
+read HOSTNAME
+sudo bash -c "echo $HOSTNAME > etc/hostname "
 
+# tmp stuff
+sudo cp /etc/resolv.conf etc
 
-cp /etc/resolv.conf etc
-
-echo deb http://http.debian.net/debian/ wheezy main contrib non-free > etc/apt/sources.list
-echo deb http://security.debian.org/ wheezy/updates main contrib non-free >> etc/apt/sources.list
+# updating root_fs
+sudo bash -c "echo deb http://http.debian.net/debian/ wheezy main contrib non-free > etc/apt/sources.list"
+sudo bash -c "echo deb http://security.debian.org/ wheezy/updates main contrib non-free >> etc/apt/sources.list"
 sudo chroot . apt-get update
 sudo chroot . apt-get upgrade
+
+# install additionnals packages
 sudo chroot . apt-get install "$PACKAGES"
 
+# removing tmp stuff
 rm etc/resolv.conf
 
-
+# add serial console to connect to the system
 echo "T0:2345:respawn:/sbin/getty -L ttyS0 115200 vt100" >> etc/inittab
+
+# copy linux image to the root_fs
 cp $LINUX_DIR/arch/arm/boot/uImage boot
 make -C $LINUX_DIR INSTALL_MOD_PATH=`pwd` ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- modules_install
