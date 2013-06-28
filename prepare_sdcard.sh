3f94eb15e5f99f04d013cac9e9fa9ab0c74c7b0a
Sylvain Leroy <sylvain@unmondelibre.fr>
2013-06-29 00:21:58 +0200

    Adding security while formating sdcard device. fdisk command not tested yet. Use with care

--- prepare_sdcard.sh
+++ prepare_sdcard.sh
@@ -16,12 +16,29 @@ set -e
 
 format_sdcard()
 {
-    if [ -n "$SDCARD_DEVICE" ]
-    then
-	dd if=/dev/zero of=$SDCARD_DEVICE bs=1k count=1025
+    # is the variable set?
+    if [ -n "$SDCARD_DEVICE" ] ;then
+	# is the file a block device?
+	if [ -b "$SDCARD_DEVICE" ] ;then
+	    # shall we format the sdcard?
+	    if [ "XYES" == "X$FORMAT_SDCARD" ]
+	    then
+		echo "n
+p
+1
+
+
+w
+" | fdisk "$SDCARD_DEVICE"
+		mkfs.ext2 "$SDCARD_DEVICE"
+	    else
+                # We should define what "correctly" means here.
+		echo "Not formating \"$SDCARD_DEVICE\" device. I assume you already did it correctly."
+	    fi
+            # format the first 1M of the ssdcard to write fresh bootloader there
+	    dd if=/dev/zero of=$SDCARD_DEVICE bs=1k count=1025
+	fi
     fi
-# MUST use something like fdisk to format the SDCARD
-    # sfdisk
 }
 
 ########
@@ -29,20 +46,22 @@ format_sdcard()
 copy2sdcard()
 {
 # copy boot binaries to the sdcard
-    if [ -n "$SDCARD_DEVICE" ] # Should match a device regexp or something like that.
-    then
-	cd u-boot-sunxi
-	sudo dd if=spl/sunxi-spl.bin of=$SDCARD_DEVICE bs=1024 seek=8
-	sudo dd if=u-boot.bin of=$SDCARD_DEVICE bs=1024 seek=32
-	cd ..
+    # Should match a device regexp or something like that.
+    if [ -n "$SDCARD_DEVICE" ] ;then
+	if [ -n "$SDCARD_DEVICE" ]
+	then
+	    cd u-boot-sunxi
+	    sudo dd if=spl/sunxi-spl.bin of=$SDCARD_DEVICE bs=1024 seek=8
+	    sudo dd if=u-boot.bin of=$SDCARD_DEVICE bs=1024 seek=32
+	    cd ..
 
 # copy root_fs to the sdcard
-	sudo mount "$SDCARD_DEVICE"1 /mnt
-	cd chroot-armhf
-	sudo bash -c "tar --exclude=qemu-arm-static -cf - . | tar -C /mnt -xvf -"
-	cd ..
-	sudo umount /mnt
-
+	    sudo mount "$SDCARD_DEVICE"1 /mnt
+	    cd chroot-armhf
+	    sudo bash -c "tar --exclude=qemu-arm-static -cf - . | tar -C /mnt -xvf -"
+	    cd ..
+	    sudo umount /mnt
+	fi
     fi
 }
 
