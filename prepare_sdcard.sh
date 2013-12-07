da81a1f313fdaefa2857359da7b4b67310929170
Sylvain Leroy <sylvain@unmondelibre.fr>
2013-12-07 23:11:32 +0100

    Removing sdcard formating stuff to update to an image generator system

--- prepare_sdcard.sh
+++ prepare_sdcard.sh
@@ -7,84 +7,109 @@ set -e
 # VARIABLES #
 #############
 
+# internal values
+BUILD_SERIAL=`date "+%Y%m%d%k%M"`
+TMP_VAL=$$
+LOOP_DEV=/dev/mapper/loop0"$TMP_VAL"
+
 # Including users defined variables
 . ./makefile.vars
+
+# user defined values
+IMG_SIZE=2048
+CONF_SIZE=50
+PARTITION_SIZE=$((($IMG_SIZE - $CONF_SIZE) / 2))
+FS_TYPE=ext2
+IMG_NAME=cubieboard2-"$BUILD_SERIAL"-"$IMG_SIZE".img
 
 ############
 # FUNCTION #
 ############
 
-format_sdcard()
+build_image()
 {
-    # is the variable set?
-    if [ -n "$SDCARD_DEVICE" ] ;then
-	# is the file a block device?
-	if [ -b "$SDCARD_DEVICE" ] ;then
-	    # shall we format the sdcard?
-	    if [ "XYES" = "X$FORMAT_SDCARD" ]
-	    then
-		# MUST BE REPLACED BY sfdisk
-		echo "n
-p
-1
+set -e
 
+if [ ! -e "$IMG_NAME" ]; then
+    dd if=/dev/zero of="$IMG_NAME" bs=1M count="$IMG_SIZE"
+fi
+/sbin/parted -s "$IMG_NAME" mklabel msdos
+/sbin/parted -s -a opt "$IMG_NAME" mkpart primary 1 $(($PARTITION_SIZE + 1))
+/sbin/parted -s -a opt "$IMG_NAME" mkpart primary $(($PARTITION_SIZE + 2)) $((($PARTITION_SIZE * 2) + 1))
+/sbin/parted -s -a opt "$IMG_NAME" mkpart primary $((($PARTITION_SIZE * 2) + 2)) $IMG_SIZE
 
-w
-" | fdisk "$SDCARD_DEVICE"
-		mkfs.ext2 "$SDCARD_DEVICE"
-	    else
-                # We should define what "correctly" means here.
-		echo "Not formating \"$SDCARD_DEVICE\" device. I assume you already did it correctly."
-	    fi
-            # format the first 1M of the sdcard to write fresh bootloader there
-	    # THIS ERASE THE PARTITION TABLE...
-            # echo "Formating the first 1M of the sdcard to write fresh bootloader there."
-	    # dd if=/dev/zero of=$SDCARD_DEVICE bs=1k count=1025
-	fi
-    fi
+set +e
+sudo /sbin/kpartx -a -v -p "$TMP_VAL" "$IMG_NAME"
+
+if [ -b "$LOOP_DEV"1 ]; then
+    /sbin/mkfs."$FS_TYPE" "$LOOP_DEV"1
+fi
+if [ -b "$LOOP_DEV"2 ]; then
+    /sbin/mkfs."$FS_TYPE" "$LOOP_DEV"2
+fi
+if [ -b "$LOOP_DEV"3 ]; then
+    /sbin/mkfs."$FS_TYPE" "$LOOP_DEV"3
+fi
+
+sudo /sbin/kpartx -d -p "$TMP_VAL" "$IMG_NAME"
+set -e
+
+/sbin/parted "$IMG_NAME" print
 }
 
 ########
 
-copyboot2sdcard()
+copyboot2image()
 {
-# copy boot binaries to the sdcard
+# copy boot binaries to the image
     # Should match a device regexp or something like that.
-    if [ -n "$SDCARD_DEVICE" ] ;then
-	if [ -b "$SDCARD_DEVICE" ]
+    if [ -n "$IMG_NAME" ] ;then
+	if [ -f "$IMG_NAME" ]
 	then
-	    cd u-boot-sunxi
-	    sudo dd if=spl/sunxi-spl.bin of=$SDCARD_DEVICE bs=1024 seek=8
-	    sudo dd if=u-boot.bin of=$SDCARD_DEVICE bs=1024 seek=32
-	    cd ..
+	    # cd u-boot-sunxi
+	    if [ -f u-boot-sunxi/spl/sunxi-spl.bin ]; then
+		sudo dd if=u-boot-sunxi/spl/sunxi-spl.bin of="$IMG_NAME" bs=1024 seek=8 conv=nocreat,notrunc
+	    else
+		echo "You need to build u-boot first"
+	    fi
+	    if [ -f u-boot-sunxi/u-boot.bin ]; then
+		sudo dd if=u-boot-sunxi/u-boot.bin of="$IMG_NAME" bs=1024 seek=32 conv=nocreat,notrunc
+	    else
+		echo "You need to build u-boot first"
+	    fi
+	    # cd ..
 	else
-	    echo "$SDCARD_DEVICE does not seem to be a block device..."
+	    echo "$IMG_NAME does not seem to be a regular image file..."
 	fi
     else
-	echo "The variable SDCARD_DEVICE does not seem to exist..."
+	echo "The variable IMG_NAME does not seem to exist..."
     fi
 }
 
 ########
 
-copyrootfs2sdcard()
+copyrootfs2image()
 {
-# copy root_fs to the sdcard
+# copy root_fs to the image
     # Should match a device regexp or something like that.
-    if [ -n "$SDCARD_DEVICE" ] ;then
-	if [ -b "$SDCARD_DEVICE" ]
+    set +e
+    sudo /sbin/kpartx -a -v -p "$TMP_VAL" "$IMG_NAME"
+    if [ -n "$LOOP_DEV"1 ] ;then
+	if [ -b "$LOOP_DEV"1 ]
 	then
-	    sudo mount "$SDCARD_DEVICE"1 /mnt
+	    sudo mount "$LOOP_DEV"1 /mnt
 	    cd chroot-armhf
 	    sudo bash -c "tar --exclude=qemu-arm-static -cf - . | tar -C /mnt -xvf -"
 	    cd ..
 	    sudo umount /mnt
 	else
-	    echo "$SDCARD_DEVICE does not seem to be a block device..."
+	    echo $LOOP_DEV"1 does not seem to be a block device..."
 	fi
     else
-	echo "The variable SDCARD_DEVICE does not seem to exist..."
+	echo "The variable LOOP_DEV does not seem to exist..."
     fi
+    sudo /sbin/kpartx -d -p "$TMP_VAL" "$IMG_NAME"
+    set -e
 }
 
 ########
@@ -93,21 +118,21 @@ copyrootfs2sdcard()
 
 case "$1" in
     all)
-	format_sdcard
-	copyboot2sdcard
-	copyrootfs2sdcard
+	build_image
+	copyboot2image
+	copyrootfs2image
 	;;
-    format)
-	format_sdcard
+    build_image)
+	build_image
 	;;
-    copyboot2sdcard)
-	copyboot2sdcard
+    copyboot2image)
+	copyboot2image
 	;;
-    copyrootfs2sdcard)
-	copyrootfs2sdcard
+    copyrootfs2image)
+	copyrootfs2image
 	;;
     *)
-	echo "Usage: prepare_sdcard.sh {all|format|copyboot2sdcard|copyrootfs2sdcard}"
+	echo "Usage: prepare_sdcard.sh {all|build_image|copyboot2image|copyrootfs2image}"
 	exit 1
 esac
 
