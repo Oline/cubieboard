b7cc1760bff9ea31e8bbe0ab7d44b5105dabfc67
Sylvain Leroy <sylvain@unmondelibre.fr>
2014-07-14 15:34:22 +0200

    Adding GPLv3 licence for the project

--- prepare_sdcard.sh
+++ prepare_sdcard.sh
@@ -1,5 +1,22 @@
 #! /usr/bin/env sh
 
+# Copyright (c) 2013-2014, Sylvain Leroy <sylvain@unmondelibre.fr>
+
+# This file is part of CBoard.
+
+# CBoard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# CBoard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with CBoard.  If not, see <http://www.gnu.org/licenses/>.
+
 #set -x
 set -e
 
